4e85814ccd24053bbc36822659da8356b9c354c3
kaseypcantu <kaseypaulcantu@gmail.com>
2020-06-02 23:45:57 -0500

    Adjusted docstrings to include typing and removed some test calls to the encrypt function.

--- hashing.py
+++ hashing.py
@@ -8,11 +8,13 @@ load_dotenv()
 
 
 def encrypt(pw: bytes) -> str:
-    """
-    Takes in a string to be encrypted.
+    """Takes in a string to be encrypted.
 
-    :param pw: A string to be encrypted (must be bytes).
-    :return: An encrypted hashed string.
+    Args:
+        pw (bytes): A string to be encrypted (must be bytes).
+
+    Returns:
+        An encrypted hashed string.
     """
     salt = bytes(os.getenv('APP_SALT'), encoding='utf-8')
     iterations = 300000
@@ -20,18 +22,15 @@ def encrypt(pw: bytes) -> str:
     return dk.hex()
 
 
-# enc = encrypt(b'test')
-# print(enc)
-# print(type(enc))
-
-
 def compare_hash(digest_a: str, digest_b: str) -> bool:
-    """
-    A utility to compare two password digests in a secure way that
-    thwarts timing attacks.
+    """A utility to compare two password digests in a secure way that thwarts
+    timing attacks.
 
-    :param digest_a: A given hashed value to be compared to another.
-    :param digest_b: The hashed value to be compared against digest_a.
-    :return: A boolean indicating if digest_a == digest_b.
+    Args:
+        digest_a (str): A given hashed value to be compared to another.
+        digest_b (str): The hashed value to be compared against digest_a.
+
+    Returns:
+        A boolean indicating if digest_a == digest_b.
     """
     return compare_digest(digest_a, digest_b)
