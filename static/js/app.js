3043593dac91129657353364df45c9b94250a267
kaseypcantu <kaseypaulcantu@gmail.com>
2020-06-08 23:30:56 -0500

    added Bulma JS to delete notifications

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,11 @@
 console.log('App.js Loaded')
 
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    let notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      notification.parentNode.removeChild(notification);
+    });
+  });
+});
