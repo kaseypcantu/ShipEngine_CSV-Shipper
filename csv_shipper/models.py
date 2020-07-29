f71810d8cf9ebd4001a49b1f1ba8097cb0d0308b
kaseypcantu <kaseypaulcantu@gmail.com>
2020-07-28 23:41:14 -0500

    Changed the Serializer to work off of the new Config file rather than just app config.

--- csv_shipper/models.py
+++ csv_shipper/models.py
@@ -5,8 +5,9 @@ from typing import List, Optional
 from sqlalchemy import func
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
-from csv_shipper import app, db, db_session, login_manager
+from csv_shipper import db, db_session, login_manager
 
 
 @dataclass
@@ -51,12 +52,12 @@ class User(db.Model, UserMixin):
     )
 
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config["SECRET_KEY"], expires_sec)
+        s = Serializer(current_app.config["SECRET_KEY"], expires_sec)
         return s.dumps({"user_id": self.id}).decode("utf-8")
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config["SECRET_KEY"])
+        s = Serializer(current_app.config["SECRET_KEY"])
         try:
             user_id = s.loads(token)["user_id"]
         except:
