d5988d744993032af201bd1a12c00b92ee03b285
kaseypcantu <kaseypaulcantu@gmail.com>
2020-07-29 00:55:09 -0500

    GraphQL View setup

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
 import logging
 
+from flask_graphql import GraphQLView
+from csv_shipper.graphql.schema import schema, User, ShippingAddress
+
 from csv_shipper import create_app, load_dotenv, db
 
 load_dotenv()
@@ -11,4 +14,12 @@ if __name__ == "__main__":
     log: logging.Logger = app.logger
     log.setLevel(logging.DEBUG)
     db.create_all()
+    app.add_url_rule(
+            '/graphql',
+            view_func=GraphQLView.as_view(
+                    'graphql',
+                    schema=schema,
+                    graphiql=True
+            )
+    )
     app.run(host="127.0.0.1", port=os.getenv("APP_PORT"), debug=True)
