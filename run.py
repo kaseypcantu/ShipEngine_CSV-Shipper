75d4e55939af0ce6050a2c1fc7275a3c34e2fa05
kaseypcantu <kaseypaulcantu@gmail.com>
2020-07-28 23:53:01 -0500

    Added db.create_all() to create all tables necessary for the database models.

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-from csv_shipper import app, db
+import os
+import logging
+
+from csv_shipper import create_app, load_dotenv, db
+
+load_dotenv()
+
+app = create_app()
 
 if __name__ == "__main__":
+    log: logging.Logger = app.logger
+    log.setLevel(logging.DEBUG)
     db.create_all()
-    app.run(host="127.0.0.1", port="9999", debug=True)
+    app.run(host="127.0.0.1", port=os.getenv("APP_PORT"), debug=True)
